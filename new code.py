44cf49ba765e58e0a4fa1fac59aa011e8dd18b84
hiranmaya14 <65801592+hiranmaya14@users.noreply.github.com>
2020-07-30 16:34:02 +0530

    Update and rename new code to new code.py
    adding code

---
+++ new code.py
@@ -0,0 +1,24 @@
+names = ["Liam",
+        "Noah",
+        "William",
+        "James",
+        "Logan",
+        "Benjamin",
+        "Mason",
+        "Elijah",
+        "Oliver",
+        "Jacob",
+        "Lucas",
+        "Michael",
+        "Alexander",
+        "Ethan"]
+start = 1
+stop = 14
+step = 2
+
+
+for i in range(start,stop,step):
+    print(names[i])
+    
+
+
